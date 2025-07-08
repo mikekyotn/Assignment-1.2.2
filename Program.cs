f1dbba0adf207f8c1b5760e8f0c4b2d5bd823e84
chank <chank@MYG5-SE>
2025-07-09 04:49:08 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment_1._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("The first 10 natural number is:");
+            for (int i = 1; i <= 10; i++)
+            {
+                sum = sum + i;
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("The Sum is: " + sum);
+        }
+    }
+}
