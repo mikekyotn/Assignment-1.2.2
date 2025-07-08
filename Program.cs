6c761c154142bdcd774a268cb8e40baf2a322287
mikekyotn <michael.chankij@mssa.newhorizons.com>
2025-07-09 06:39:41 +0900

    change from 10numbers to user inputted

--- Program.cs
+++ Program.cs
@@ -4,15 +4,18 @@
     {
         static void Main(string[] args)
         {
-            int sum = 0;
-            Console.WriteLine("The first 10 natural number is:");
-            for (int i = 1; i <= 10; i++)
+            int sumNaturalNumbers = 0;
+            Console.WriteLine("This program sums the number of natural numbers you define.");
+            Console.WriteLine("Enter how many natural numbers you want to sum:");
+            int totalNaturalNumbers = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The first " +totalNaturalNumbers+ " natural number are:");
+            for (int i = 1; i <= totalNaturalNumbers; i++)
             {
-                sum = sum + i;
+                sumNaturalNumbers = sumNaturalNumbers + i;
                 Console.Write(i + " ");
             }
             Console.WriteLine();
-            Console.WriteLine("The Sum is: " + sum);
+            Console.WriteLine("The Sum is: " + sumNaturalNumbers);
         }
     }
 }
